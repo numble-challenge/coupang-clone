1fb2f3ee87a56870ea37fe1c9ebe04109e866c62
numble-challenger <numble.challenge@gmail.com>
2022-06-30 20:17:11 +0900

    feat(): add sample LoginPage, SignupPage

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,3 +1,24 @@
+import styled from "@emotion/styled";
+import { useForm } from "react-hook-form";
+
 export default function LoginPage() {
-    return <></>
+  const { register, handleSubmit } = useForm<{
+    email: string;
+    password: string;
+  }>();
+
+  const onSubmit = () => {};
+
+  return (
+    <Wrapper>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("email")} />
+        <input {...register("password")} />
+        <button type="submit">로그인</button>
+        <a href="/auth/signup">회원가입</a>
+      </form>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
---
+++ pages/auth/signup.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export default function SignupPage() {
+  return <Wrapper></Wrapper>;
+}
+
+const Wrapper = styled.div``;
