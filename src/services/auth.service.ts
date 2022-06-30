ba8d2cc6f4830ea155246490ccb8e0823d1b5f61
numble-challenger <numble.challenge@gmail.com>
2022-06-30 20:38:54 +0900

    enhance(services): update SignupAgreements

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,14 +2,24 @@ import axios from "axios";
 import cookies from "js-cookie";
 
 type SignupAgreements = {
-  privacy: boolean;
-  ad:
-    | {
-        email: boolean;
-        sms: boolean;
-        app: boolean;
-      }
-    | false;
+  /** 만 14세 이상입니다 */
+  terms_fourteen: boolean;
+  /** 쿠팡 이용약관 동의 */
+  terms_service: boolean;
+  /** 전자금융거래 이용약관 동의 */
+  terms_commerce: boolean;
+  /** 개인정보 수집 및 이용 동의 */
+  terms_privacy_collect_use: boolean;
+  /** 개인정보 제3차 제공 동의 */
+  agree_to_collect_third_part_information: boolean;
+  /** 광고성 목적의 개인정보 수집 및 이용 동의 */
+  agree_to_collect_for_ads: boolean;
+  /** 이메일 수신 동의 */
+  agree_to_receive_email: boolean;
+  /** SMS,MMS 수신 동의 */
+  agree_to_receive_sms: boolean;
+  /** 앱 푸시 수신 동의 */
+  agree_to_receive_push: boolean;
 };
 
 class AuthService {
